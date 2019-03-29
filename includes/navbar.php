1161027d0bd29ee4cefab6f75436f95927afb13a
yuvraj034c <yuvraj108c@gmail.com>
2019-03-29 17:02:18 +0400

    Created navbar

---
+++ includes/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+    <div class="container">
+
+        <!-- Logo -->
+        <a class="navbar-brand" href="#">InovApp</a>
+
+        <!-- Burger icon -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Navbar content -->
+        <div class="collapse navbar-collapse" id="navbarContent">
+
+            <ul class="navbar-nav mr-auto"></ul>
+
+            <!-- Login button -->
+            <a class="btn btn-outline-success" href="#" role="button">Login</a>
+
+        </div>
+    </div>
+
+</nav>

60d002c1dd91c175c0e22240a499366a9b73bc9d
yuvraj034c <yuvraj108c@gmail.com>
2019-03-29 17:02:48 +0400

    Created header

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<?php
+
+echo
+"<!doctype html>
+<html lang='en'>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+
+    <!-- Bootstrap CSS -->
+    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
+
+    <title>Homepage</title>
+</head>
+
+<body>";

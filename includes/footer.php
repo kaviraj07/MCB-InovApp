3d6fe5a98bf2a96fe38412292b70c7b2fe1e0cf1
yuvraj034c <yuvraj108c@gmail.com>
2019-03-29 20:43:24 +0400

    Added footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,51 @@
 <?php
 
-echo "    
+echo "
+<footer id='myFooter'>
+        <div class='container'>
+            <div class='row'>
+                <div class='col-sm-3'>
+                    <h5>Get started</h5>
+                    <ul>
+                        <li><a href='#'>Home</a></li>
+                        <li><a href='#'>Sign up</a></li>
+                        <li><a href='#'>Downloads</a></li>
+                    </ul>
+                </div>
+                <div class='col-sm-3'>
+                    <h5>About us</h5>
+                    <ul>
+                        <li><a href='#'>Company Information</a></li>
+                        <li><a href='#'>Contact us</a></li>
+                        <li><a href='#'>Reviews</a></li>
+                    </ul>
+                </div>
+                <div class='col-sm-3'>
+                    <h5>Support</h5>
+                    <ul>
+                        <li><a href='#'>FAQ</a></li>
+                        <li><a href='#'>Help desk</a></li>
+                        <li><a href='#'>Forums</a></li>
+                    </ul>
+                </div>
+                <div class='col-sm-3 info'>
+                    <h5>Information</h5>
+                    <p> Lorem ipsum dolor amet, consectetur adipiscing elit. Etiam consectetur aliquet aliquet. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
+                </div>
+            </div>
+        </div>
+        <div class='second-bar'>
+           <div class='container'>
+                <h2 class='logo'><a href='#'> InovApp </a></h2>
+                <div class='social-icons'>
+                    <a href='#' class='twitter'><i class='fa fa-twitter'></i></a>
+                    <a href='#' class='facebook'><i class='fa fa-facebook'></i></a>
+                    <a href='#' class='google'><i class='fa fa-google-plus'></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>   
+     
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js'></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'></script>

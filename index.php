cfe8b58f0198258b2edd12aa23fb70b039dd8d47
yuvraj034c <yuvraj108c@gmail.com>
2019-03-29 17:31:59 +0400

    Created jumbotron

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    include("includes/header.php");
+    include("includes/navbar.php");
+?>
+
+<div class="container">
+
+    <!-- Jumbotron -->
+    <section class="jumbotron">
+        <h1 class="display-4">Mauritius</h1>
+        <p class="lead">A platform for promoting mauritian culture.</p>
+        <hr class="my-4">
+        <a class="btn btn-success btn-lg" href="#" role="button">Explore</a>
+    </section>
+
+</div>
+
+<?php include("includes/footer.php"); ?>

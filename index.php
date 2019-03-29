a9f1d025ab2eb23ded514865d64bae0bd53edfd4
yuvraj034c <yuvraj108c@gmail.com>
2019-03-29 18:35:13 +0400

    Added cards for categories

--- index.php
+++ index.php
@@ -8,9 +8,51 @@
     <!-- Jumbotron -->
     <section class="jumbotron">
         <h1 class="display-4">Mauritius</h1>
-        <p class="lead">A platform for promoting mauritian culture.</p>
+        <p class="lead">A platform for promoting and preserving our mauritian culture.</p>
         <hr class="my-4">
-        <a class="btn btn-success btn-lg" href="#" role="button">Explore</a>
+        <a class="btn btn-success btn-lg" href="#" role="button">Explore now</a>
+    </section>
+
+    <section id="cards">
+        <div class="row">
+
+            <!-- Card 1 -->
+            <div class="col-sm">
+                <div class="card" style="width: 100%;">
+                    <img src="assets/images/food.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Learn about the unique food culture of Mauritius. We have got all sorts of
+                            mauritians food recipes ranging from dholl puri to briani. Become a mauritian cook.</p>
+                        <a href="#" class="card-link">Learn more</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Card 1 -->
+            <div class="col-sm">
+                <div class="card" style="width: 100%;">
+                    <img src="assets/images/music.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Learn about the music culture of Mauritius. Experience like never before
+                            the Sega and geet gawai. Listen to songs and watch videos made by mauritians.
+                        </p>
+                        <a href="#" class="card-link">Learn more</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Card 1 -->
+            <div class="col-sm">
+                <div class="card" style="width: 100%;">
+                    <img src="assets/images/lemorne.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Discover cultural places in Mauritius. From temples to museums, you will
+                            learn about the enriching history of Mauritius. Prepare to dive into the shoes of a local
+                            explorer.</p>
+                        <a href="#" class="card-link">Learn more</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </section>
 
 </div>
